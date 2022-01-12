3d24fb89a64934447bd4a7943b0f202a91649296
Koval Yaroslav <koval@q-writer.com>
2022-01-12 22:39:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ const client = mc_client.createClient({
   auth: 'mojang'
 })
 client.on('chat', function(packet) {
-  // if (chat_array.length > max_len_chat_array) { chat_array.slice(-Math.abs(max_len_chat_array)) }
-  // chat_array.push({"raw_msg": JSON.parse(packet.message), "time_order": Math.floor(new Date() / 1000)})
-  chat_array.push(JSON.parse(packet.message))
+  if (chat_array.length > max_len_chat_array) { chat_array.slice(-Math.abs(max_len_chat_array)) }
+  chat_array.push({"raw_msg": JSON.parse(packet.message), "time_order": Math.floor(new Date() / 1000)})
 })
 
 app.set('port', (process.env.PORT || 5000))
