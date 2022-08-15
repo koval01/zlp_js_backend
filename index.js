1b156c1323ce1d0793cb9e032be3702b6a79d517
Koval Yaroslav <koval@q-writer.com>
2022-08-15 15:22:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const mcstatus = require('minecraft-server-util')
 const app = express()
 
 const limiter = rateLimit({
-	windowMs: 60 * 1000, // 1 minute
-	max: 100, // Max 100 requests per 1 minute
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: 60 * 1000, // 1 minute
+    max: 100, // Max 100 requests per 1 minute
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 app.set('port', (process.env.PORT || 5000))
@@ -22,120 +22,120 @@ app.use(cors())
 app.use(limiter)
 
 app.get('/channel', (req, resp) => {
-  try {
-    const choice_ = ['zalupa_history', 'zalupaonline']
-    request(
-      {
-        uri: `https://t.me/s/${choice_[req.query.choice]}?before=${req.query.before}`,
-        method: 'POST',
-        headers: {
-          Origin: 'https://t.me',
-          Referer: `https://t.me/s/${choice_[req.query.choice]}`,
-          Host: 't.me',
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15',
-          'X-Requested-With': 'XMLHttpRequest',
-          Connection: 'keep-alive'
-        }
-      },
-      (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          body = body.toString().replace(/\\/gm, "")
-          const regex = /data-post="[A-z\d_-]*\/[\d]*"/gm
-          const matched = body.match(regex)
-          resp.send({ 
-            success: true, 
-            last_post: matched[matched.length - 1].match(/data-post="([A-z\d_-]*\/[\d]*)"/)[1]
-          })
-        } else {
-          resp.send({ success: false, message: 'Input function error', exception: error })
-        }
-      }
-    )
-  } catch (error) {
-    resp.send({
-      success: false,
-      error_body: {
-        message: 'Global function error', exception: error
-      }
-    })
-  }
+    try {
+        const choice_ = ['zalupa_history', 'zalupaonline']
+        request(
+            {
+                uri: `https://t.me/s/${choice_[req.query.choice]}?before=${req.query.before}`,
+                method: 'POST',
+                headers: {
+                    Origin: 'https://t.me',
+                    Referer: `https://t.me/s/${choice_[req.query.choice]}`,
+                    Host: 't.me',
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    Connection: 'keep-alive'
+                }
+            },
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    body = body.toString().replace(/\\/gm, "")
+                    const regex = /data-post="[A-z\d_-]*\/[\d]*"/gm
+                    const matched = body.match(regex)
+                    resp.send({
+                        success: true,
+                        last_post: matched[matched.length - 1].match(/data-post="([A-z\d_-]*\/[\d]*)"/)[1]
+                    })
+                } else {
+                    resp.send({ success: false, message: 'Input function error', exception: error })
+                }
+            }
+        )
+    } catch (error) {
+        resp.send({
+            success: false,
+            error_body: {
+                message: 'Global function error', exception: error
+            }
+        })
+    }
 })
 
 app.get('/donate/services', (req, resp) => {
-  try {
-    request(
-      {
-        uri: `https://easydonate.ru/api/v3/shop/products`,
-        method: 'GET',
-        headers: {
-          'Shop-Key': process.env.DONATE_API_KEY
-        }
-      },
-      (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          body = JSON.parse(body)
-					if (body.success) {
-						resp.send({ 
-							success: true, 
-							services: body
-						})
-					}
-          resp.send({ 
-            success: false, 
-            message: "Error check response EasyDonate API",
-						exception: "var success is not true"
-          })
-        } else {
-          resp.send({ success: false, message: 'Input function error', exception: error })
-        }
-      }
-    )
-  } catch (error) {
-    resp.send({
-      success: false,
-      error_body: {
-        message: 'Global function error', exception: error
-      }
-    })
-  }
+    try {
+        request(
+            {
+                uri: `https://easydonate.ru/api/v3/shop/products`,
+                method: 'GET',
+                headers: {
+                    'Shop-Key': process.env.DONATE_API_KEY
+                }
+            },
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    body = JSON.parse(body)
+                    if (body.success) {
+                        resp.send({
+                            success: true,
+                            services: body
+                        })
+                    }
+                    resp.send({
+                        success: false,
+                        message: "Error check response EasyDonate API",
+                        exception: "var success is not true"
+                    })
+                } else {
+                    resp.send({ success: false, message: 'Input function error', exception: error })
+                }
+            }
+        )
+    } catch (error) {
+        resp.send({
+            success: false,
+            error_body: {
+                message: 'Global function error', exception: error
+            }
+        })
+    }
 })
 
 app.get('/server', (req, resp) => {
-  try {
-    const options = {
-      timeout: 1000 * 3
-    }
-    function result_(data) {
-      return {
-	"online": data.players.online,
-	"latency": data.roundTripLatency
-      }
-    }
-    mcstatus.status('zalupa.online', 25565, options)
-      .then((result) => resp.send({ 
-        success: true, body: result_(result) 
-      }))
-      .catch((error) => resp.send({
-        success: false,
-        error_body: {
-          message: 'Server data get error', exception: error
+    try {
+        const options = {
+            timeout: 1000 * 3
         }
-      }))
-  } catch (error) {
-    resp.send({
-      success: false,
-      error_body: {
-        message: 'Global function error', exception: error
-      }
-    })
-  }
+        function result_(data) {
+            return {
+                "online": data.players.online,
+                "latency": data.roundTripLatency
+            }
+        }
+        mcstatus.status('zalupa.online', 25565, options)
+            .then((result) => resp.send({
+                success: true, body: result_(result)
+            }))
+            .catch((error) => resp.send({
+                success: false,
+                error_body: {
+                    message: 'Server data get error', exception: error
+                }
+            }))
+    } catch (error) {
+        resp.send({
+            success: false,
+            error_body: {
+                message: 'Global function error', exception: error
+            }
+        })
+    }
 })
 
 app.listen(app.get('port'), () => {
-  console.log(`Node app is running at localhost:${app.get('port')}`)
+    console.log(`Node app is running at localhost:${app.get('port')}`)
 })
 
 process.on('uncaughtException', function (exception) {
-  console.error(`Uncaught exception: ${exception}`)
+    console.error(`Uncaught exception: ${exception}`)
 })
 
