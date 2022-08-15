2168684fe274ac4276664fcd36f5e4a17196e551
Koval Yaroslav <koval@q-writer.com>
2022-08-15 15:49:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ app.use(compression())
 app.use(cors())
 app.use(limiter)
 
+function url_builder_(base_url, submit_data_) {
+    let url = URL(base_url)
+    for (let i = 0; i < submit_data_.length; i++) {
+        url.searchParams.set(submit_data_[i].name, submit_data_[i].value)
+    }
+    return url.href
+}
+
 app.get('/channel', (req, resp) => {
     try {
         const choice_ = ['zalupa_history', 'zalupaonline']
@@ -96,6 +104,56 @@ app.get('/donate/services', (req, resp) => {
                         resp.send({
                             success: true,
                             services: response_(body.response)
+                        })
+                    }
+                    resp.send({
+                        success: false,
+                        message: "Error check response EasyDonate API",
+                        exception: "var success is not true"
+                    })
+                } else {
+                    resp.send({ success: false, message: 'Input function error', exception: error })
+                }
+            }
+        )
+    } catch (error) {
+        resp.send({
+            success: false,
+            error_body: {
+                message: 'Global function error', exception: error
+            }
+        })
+    }
+})
+
+app.get('/donate/payment/create', (req, resp) => {
+    try {
+        let json_body = req.body
+        let url = url_builder_(
+            'https://easydonate.ru/api/v3/shop/payment/create',
+            [
+                { "name": "customer", "value": json_body["customer"] },
+                { "name": "server_id", "value": process.env.SERVER_ID },
+                { "name": "products", "value": json_body["products"] },
+                { "name": "email", "value": json_body["email"] },
+                { "name": "coupon", "value": json_body["coupon"] }
+            ]
+        )
+        request(
+            {
+                uri: url,
+                method: 'GET',
+                headers: {
+                    'Shop-Key': process.env.DONATE_API_KEY
+                }
+            },
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    body = JSON.parse(body)
+                    if (body.success) {
+                        resp.send({
+                            success: true,
+                            payment_url: body.response.url
                         })
                     }
                     resp.send({
