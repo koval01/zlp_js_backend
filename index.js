a73cdd0b13be0af01f981a87ba896b217c2fe5e2
Koval Yaroslav <koval@q-writer.com>
2022-08-16 21:20:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,14 @@ const compression = require('compression')
 const cors = require('cors')
 const html_parser = require('node-html-parser')
 const express = require('express')
-const rateLimit = require('express-rate-limit')
 const mcstatus = require('minecraft-server-util')
 
 const app = express()
-
-const limiter = rateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 100, // Max 100 requests per 1 minute
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.json())
 app.use(compression())
 app.use(cors())
-app.use(limiter)
 
 function url_builder_(base_url, submit_data_) {
     let url = new URL(base_url)
