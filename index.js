1d9fba1dd49608b01cfbd9301c30a65820abdf65
Koval Yaroslav <koval@q-writer.com>
2022-08-24 23:05:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -185,72 +185,72 @@ app.get('/donate/services', (req, resp) => {
 
 app.post('/donate/coupon', (req, resp) => {
     const json_body = req.body
-        if (reccheck(json_body.token)) {
-            try {
-                function response_(data) {
-                    if (data) {
-                        let products = data.products
-                        let products_list = []
-                        for (let i = 0; i < products.length; i++) {
-                            products_list.push({
-                                "id": products[i].id,
-                                "name": products[i].name
-                            })
-                        }
-                        return {
-                            "code": data.code,
-                            "discount": data.sale,
-                            "products": products_list
+    if (reccheck(json_body.token)) {
+        try {
+            function response_(data) {
+                if (data) {
+                    let products = data.products
+                    let products_list = []
+                    for (let i = 0; i < products.length; i++) {
+                        products_list.push({
+                            "id": products[i].id,
+                            "name": products[i].name
+                        })
+                    }
+                    return {
+                        "code": data.code,
+                        "discount": data.sale,
+                        "products": products_list
 
-                        }
-                    } else {
-                        return null
                     }
-                }
-                function select_coupon(data, name) {
-                    if (data) {
-                        for (let i = 0; i < data.length; i++) {
-                            if (data[i].code === name) {
-                                return data[i]
-                            }
-                        }
-                    }
+                } else {
                     return null
                 }
-                request(
-                    {
-                        uri: `https://easydonate.ru/api/v3/shop/coupons?where_active=true`,
-                        method: 'GET',
-                        headers: {
-                            'Shop-Key': process.env.DONATE_API_KEY
-                        }
-                    },
-                    (error, response, body) => {
-                        if (!error && response.statusCode == 200) {
-                            body = JSON.parse(body)
-                            if (body.success) {
-                                resp.send({
-                                    success: true,
-                                    coupon: response_(select_coupon(body.response, json_body.code))
-                                })
-                            }
-                            resp.send({
-                                success: false,
-                                message: "Error check response EasyDonate API",
-                                exception: "var success is not true"
-                            })
-                        } else {
-                            resp.send({ success: false, message: 'Input function error', exception: error })
+            }
+            function select_coupon(data, name) {
+                if (data) {
+                    for (let i = 0; i < data.length; i++) {
+                        if (data[i].code === name) {
+                            return data[i]
                         }
                     }
-                )
-            } catch (error) {
-                resp.send({
-                    success: false,
-                    message: 'Main function error', 
-                    exception: error
-                })
+                }
+                return null
             }
+            request(
+                {
+                    uri: `https://easydonate.ru/api/v3/shop/coupons?where_active=true`,
+                    method: 'GET',
+                    headers: {
+                        'Shop-Key': process.env.DONATE_API_KEY
+                    }
+                },
+                (error, response, body) => {
+                    if (!error && response.statusCode == 200) {
+                        body = JSON.parse(body)
+                        if (body.success) {
+                            resp.send({
+                                success: true,
+                                coupon: response_(select_coupon(body.response, json_body.code))
+                            })
+                        }
+                        resp.send({
+                            success: false,
+                            message: "Error check response EasyDonate API",
+                            exception: "var success is not true"
+                        })
+                    } else {
+                        resp.send({ success: false, message: 'Input function error', exception: error })
+                    }
+                }
+            )
+        } catch (error) {
+            resp.send({
+                success: false,
+                message: 'Main function error', 
+                exception: error
+            })
+        }
     } else {
         resp.send({
             success: false,
@@ -262,55 +262,55 @@ app.post('/donate/coupon', (req, resp) => {
 
 app.post('/donate/payment/create', (req, resp) => {
     let json_body = req.body
-        if (reccheck(json_body.token)) {
-            try {
-                let url = url_builder_(
-                    'https://easydonate.ru/api/v3/shop/payment/create',
-                    [
-                        { "name": "customer", "value": json_body["customer"] },
-                        { "name": "server_id", "value": process.env.SERVER_ID },
-                        { "name": "products", "value": JSON.stringify(json_body["products"]) },
-                        { "name": "email", "value": json_body["email"] },
-                        { "name": "coupon", "value": json_body["coupon"] }
-                    ]
-                )
-                request(
-                    {
-                        uri: url,
-                        method: 'GET',
-                        headers: {
-                            'Shop-Key': process.env.DONATE_API_KEY
-                        }
-                    },
-                    (error, response, body) => {
-                        if (!error && response.statusCode == 200) {
-                            body = JSON.parse(body)
-                            if (body.success) {
-                                resp.send({
-                                    success: true,
-                                    payment: {
-                                        "url": body.response.url,
-                                        "bill_id": body.response.payment.id
-                                    } 
-                                })
-                            }
-                            resp.send({
-                                success: false,
-                                message: "Error check response EasyDonate API",
-                                exception: "var success is not true"
-                            })
-                        } else {
-                            resp.send({ success: false, message: 'Input function error', exception: error })
-                        }
+    if (reccheck(json_body.token)) {
+        try {
+            let url = url_builder_(
+                'https://easydonate.ru/api/v3/shop/payment/create',
+                [
+                    { "name": "customer", "value": json_body["customer"] },
+                    { "name": "server_id", "value": process.env.SERVER_ID },
+                    { "name": "products", "value": JSON.stringify(json_body["products"]) },
+                    { "name": "email", "value": json_body["email"] },
+                    { "name": "coupon", "value": json_body["coupon"] }
+                ]
+            )
+            request(
+                {
+                    uri: url,
+                    method: 'GET',
+                    headers: {
+                        'Shop-Key': process.env.DONATE_API_KEY
                     }
-                )
-            } catch (error) {
-                resp.send({
-                    success: false,
-                    message: 'Main function error', 
-                    exception: error
-                })
-            }
+                },
+                (error, response, body) => {
+                    if (!error && response.statusCode == 200) {
+                        body = JSON.parse(body)
+                        if (body.success) {
+                            resp.send({
+                                success: true,
+                                payment: {
+                                    "url": body.response.url,
+                                    "bill_id": body.response.payment.id
+                                } 
+                            })
+                        }
+                        resp.send({
+                            success: false,
+                            message: "Error check response EasyDonate API",
+                            exception: "var success is not true"
+                        })
+                    } else {
+                        resp.send({ success: false, message: 'Input function error', exception: error })
+                    }
+                }
+            )
+        } catch (error) {
+            resp.send({
+                success: false,
+                message: 'Main function error', 
+                exception: error
+            })
+        }
     } else {
         resp.send({
             success: false,
