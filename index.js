2420a733beef2bd381cedde4fa5f26c50d0b5048
Koval Yaroslav <koval@q-writer.com>
2022-09-07 00:29:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,20 +37,24 @@ function logError(err, req, res, next) {
 }
 app.use(logError)
 
-const con = mysql.createConnection({
-    host: process.env.DB_HOSTNAME,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    ssl: false
-})
+const mysql_ = function() {
+    return cursor = mysql.createConnection({
+        host: process.env.DB_HOSTNAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        ssl: false
+    })
+}
 
 function sql_request(callback, query, values = []) {
     let error = (e) => logger.error(`Database error: ${e}`)
+    let con = mysql_()
     con.query(query, values, 
         function (err, result, _) {
             if (err) error(err)
             callback(result)
+            con.end()
     })
 }
 
