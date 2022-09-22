583acfda0365b46e83b353e3a943a5bb9474bee3
Koval Yaroslav <koval@q-writer.com>
2022-09-23 00:36:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ app.get('/events', (req, resp) => {
                     let result = []
                     for (let i = 0; i < messages.length; i++) {
                         let container = messages[i]
-                        let text_post = container.querySelector(".tgme_widget_message_text").innerHTML
+                        let text_post = container.querySelector(".tgme_widget_message_text").innerHTML.toString()
                         if (text_post.length) {
                             let parsed_ = text_post.match(message_regex)
                             if (parsed_) {
