4b175cf7c65009c111e23b8954977f6112bc2181
Koval Yaroslav <koval@q-writer.com>
2022-09-23 01:07:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ app.get('/events', (req, resp) => {
                                     let defined_date_st = new Date(`20${date_st[3]}`, date_st[2] - 1, date_st[1], date_st[4], date_st[5], '00')
                                     let defined_date_end = new Date(`20${date_end[3]}`, date_end[2] - 1, date_end[1], date_end[4], date_end[5], '00')
                                     let to_start = ((defined_date_st - time_in_moscow) / 1000)
-                                    let to_end = ((defined_date_end - time_in_moscow) / 1000)
+                                    let to_end = ((time_in_moscow - time_in_moscow) / 1000)
                                     logger.info(to_start)
                                     logger.info(to_end)
                                     if (to_start < 259200 && to_end < 259200) {
