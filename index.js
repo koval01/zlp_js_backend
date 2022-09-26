9e5d0b28be79bea57f9eeea26e821f325246c3f8
Koval Yaroslav <yaroslav@koval.page>
2022-09-27 01:09:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -286,7 +286,6 @@ app.post('/channel_parse', reccheck, async (req, resp) => {
 })
 
 app.post('/events', reccheck, async (req, resp) => {
-    req.body
     try {
         request(
             {
@@ -322,7 +321,7 @@ app.post('/events', reccheck, async (req, resp) => {
                                     let defined_date_end = new Date(`20${date_end[3]}`, date_end[2] - 1, date_end[1], date_end[4], date_end[5], '00')
                                     let to_start = ((defined_date_st - time_in_moscow) / 1000)
                                     let to_end = ((time_in_moscow - time_in_moscow) / 1000)
-                                    if (to_start < 259200 && to_end > 259200) {
+                                    if (to_start < 259200 || to_end > 259200) {
                                         result.push({
                                             title: parsed_[1],
                                             date_start: defined_date_st.toJSON(),
@@ -819,7 +818,6 @@ app.post('/crypto', reccheck, async (req, resp) => {
 })
 
 app.post('/server', crypto_check, async (req, resp) => {
-    req.body
     try {
         let options = {
             timeout: 1000 * 2
