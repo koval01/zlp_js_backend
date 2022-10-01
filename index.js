4625c5744f606ad8f67bca34ebac1d08c4eaccf7
Koval Yaroslav <yaroslav@koval.page>
2022-10-01 23:44:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(compression())
 app.use(cors())
 
 function logRequest(req, res, next) {
-    console.log(`Request: ${req.url}`)
+    console.log(`Request: [${req.method}] ${req.url}`)
     next()
 }
 app.use(logRequest)
