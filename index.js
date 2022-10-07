decf90a818a7e214ea96a24b8b3952f42fa0ce28
Koval Yaroslav <yaroslav@koval.page>
2022-10-08 00:01:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -338,7 +338,7 @@ app.post('/events', reccheck, async (req, resp) => {
                                     
                                     console.log(`
                                         ${i} // ${parsed_[1]} :
-                                        ${defined_date_st} ${time_in_moscow}
+                                        STR: ${defined_date_st} END: ${defined_date_end} MSC: ${time_in_moscow}
                                         S:${to_start} (${to_start > 0 || -(to_start) < 259200} (${to_start > 0} / ${-(to_start) > 259200})) 
                                         E:${to_end} (${-(to_end) > 0 && to_end < 259200} (${-(to_end) > 0} / ${to_end < 259200})) 
                                     `)
