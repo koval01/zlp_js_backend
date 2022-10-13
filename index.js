b73120ef53497512d7fb166e34b78e063f049993
Koval Yaroslav <yaroslav@koval.page>
2022-10-13 22:59:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ app.post('/channel_get', rateLimit({
                     },
                     (error, response, body) => {
                         if (!error && response.statusCode == 200) {
-                            console.log(body.toString())
+                            console.log(body.toString().replace(/(\r\n|\n|\r)/gm, ""))
                             body = body.toString().replace(/\\/gm, "")
                             let messages = html_parser.parse(body).querySelectorAll(".tgme_widget_message_wrap")
                             if (!req.query.offset) {
