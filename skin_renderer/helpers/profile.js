500a2b029cbcf3bc30a8945f0079ee70de043b85
Koval Yaroslav <yaroslav@koval.page>
2022-10-21 18:21:47 +0200

    change width for body render

--- skin_renderer/helpers/profile.js
+++ skin_renderer/helpers/profile.js
@@ -164,7 +164,7 @@ async function renderBody64(skinBuffer, width = 160, height = 320, isSlim = fals
 
 async function get3DSkin(texture) {
     const skinB64 = await getSkin64(texture);
-    const skin = new MinecraftSkin(Buffer.from(skinB64, "base64"), false, 120);
+    const skin = new MinecraftSkin(Buffer.from(skinB64, "base64"), false, 320);
     return skin.getRender();
 }
 
