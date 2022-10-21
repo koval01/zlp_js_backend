5f3fd3501e7f6b26a2ff013b8302fa8f79961653
Koval Yaroslav <yaroslav@koval.page>
2022-10-21 16:14:50 +0200

    Update head.js

--- skin_renderer/controller/head.js
+++ skin_renderer/controller/head.js
@@ -9,9 +9,7 @@ module.exports.getHead = async (req, res) => {
     //     return res.status(400)
     // }
 
-    console.log("pos 2")
-    // const texture = req.params.texture_hash;
-    const texture = "c7acd6b5a7a61bb25892225d160138b4e97fa02cc33fbecb559deae4cb485f" // for test
+    const texture = req.params.texture_hash
     const width = Numbers.getPositive(req.params.width, 80);
     const height = Numbers.getPositive(req.params.height, 80);
 
@@ -19,22 +17,16 @@ module.exports.getHead = async (req, res) => {
         return res.status(400)
     }
 
-    console.log("pos 3")
-
     let head64 = await getHead64(texture, width, height, req.params.overlay);
 
-    console.log("pos 4")
     head64 = head64.substr(head64.indexOf(",") + 1);
 
-    console.log("pos 5")
     if (req.params.base64) {
         return res.send(head64);
     }
 
-    console.log("pos 6")
     const head = Buffer.from(head64, "base64");
 
-    console.log("pos 7")
     res.set("Content-Type", "image/png");
     return res.send(head);
 };
