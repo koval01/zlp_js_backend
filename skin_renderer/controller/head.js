0fce3b9be0b46a18c45fef1c12b364daed6ab659
Koval Yaroslav <yaroslav@koval.page>
2022-10-21 15:45:21 +0200

    Update head.js

--- skin_renderer/controller/head.js
+++ skin_renderer/controller/head.js
@@ -2,12 +2,13 @@ const { getHead64 } = require("../helpers/profile")
 const { getVerifiedTelegramData } = require("../../telegram")
 
 module.exports.getHead = async (req, res) => {
-    console.log(req, res)
+    console.log("pos 1")
     const tg_user = getVerifiedTelegramData(req.params.tg_auth, custom_var=true)
     if (!tg_user) {
         return res.status(400)
     }
 
+    console.log("pos 2")
     // const texture = req.params.texture_hash;
     const texture = "c7acd6b5a7a61bb25892225d160138b4e97fa02cc33fbecb559deae4cb485f" // for test
     const width = Numbers.getPositive(req.options.width, 80);
@@ -17,16 +18,22 @@ module.exports.getHead = async (req, res) => {
         return res.status(400)
     }
 
+    console.log("pos 3")
+
     let head64 = await getHead64(texture, width, height, req.options.overlay);
 
+    console.log("pos 4")
     head64 = head64.substr(head64.indexOf(",") + 1);
 
+    console.log("pos 5")
     if (req.options.base64) {
         return res.send(head64);
     }
 
+    console.log("pos 6")
     const head = Buffer.from(head64, "base64");
 
+    console.log("pos 7")
     res.set("Content-Type", "image/png");
     return res.send(head);
 };
