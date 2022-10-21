defa86641706f56bd3b544bd31bd8b6d86949091
Koval Yaroslav <yaroslav@koval.page>
2022-10-21 20:40:27 +0200

    part refactoring

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const {global_error} = require("./middleware/other_middle")
 const {re_check, tg_check} = require("./middleware/security_middle")
 const {input_e, main_e} = require("./helpers/errors")
 const {monitoring_statistic} = require("./database/functions/monitoring")
+const {promotions_sql} = require("./database/functions/promotion")
 
 const static_view = require("./static")
 const {crypto_view, crypto_check} = require("./helpers/crypto")
@@ -387,61 +388,7 @@ app.post('/promotion', async (req, resp) => {
         monitoring_statistic(get_mon_()["name"], body.username)
     }
 
-    sql_request(function (result) {
-            let error = () => resp.send("Ошибка базы данных")
-            let no_player = () => resp.send("Игрок не найден")
-            if (!result) {
-                return error()
-            } else if (!result.length) {
-                return no_player()
-            } else if (!result[0].uuid) {
-                return no_player()
-            } else {
-                let add_permission = () => {
-                    sql_request(function (insert_result) {
-                            if (insert_result) {
-                                stat()
-                                console.log(`Result insert to luckperms : ${JSON.stringify(insert_result)}`)
-                                return resp.send("ok")
-                            }
-                            return error
-                        },
-                        "INSERT luckperms_user_permissions (uuid, permission, value, server, world, expiry, contexts) VALUES (?, ?, 1, 'global', 'global', '0', '{}')",
-                        [result[0].uuid, permission_ident]
-                    )
-                }
-                let update_permission = () => {
-                    sql_request(function (update_result) {
-                            if (update_result) {
-                                stat()
-                                console.log(`Result update row in luckperms : ${JSON.stringify(update_result)}`)
-                                return resp.send("ok")
-                            }
-                            return error
-                        },
-                        "UPDATE luckperms_user_permissions SET `value` = 1 WHERE `uuid` = ? AND `permission` = ? ORDER BY id DESC LIMIT 1",
-                        [result[0].uuid, permission_ident]
-                    )
-                }
-                sql_request(function (permission) {
-                        if (!permission.length) {
-                            return add_permission()
-                        } else if (parseInt(permission[0].value) !== 1) {
-                            return update_permission()
-                        } else if (parseInt(permission[0].value) === 1) {
-                            return resp.send("Право уже выдано")
-                        }
-                        return resp.send("Неизвестная ошибка")
-                    },
-                    "SELECT `uuid`, `permission`, `value` FROM luckperms_user_permissions WHERE `uuid` = ? AND `permission` = ? ORDER BY id DESC LIMIT 1",
-                    [result[0].uuid, permission_ident]
-                )
-            }
-            return error
-        },
-        "SELECT `uuid` FROM `luckperms_players` WHERE `username` = ?",
-        [body.username]
-    )
+    promotions_sql(resp, body, stat(), permission_ident)
 })
 
 app.post('/donate/services', rateLimit({
