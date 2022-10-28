89214096022c02f60b71b40c3cfbbfd791c499e8
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-10-28 15:03:37 +0200

    part refactoring

--- helpers/porfirevich.js
+++ helpers/porfirevich.js
@@ -13,15 +13,22 @@ const getResponsePorfirevich = async (text) => {
 const getPorfirevich = async (text = "Вам нужно явится на Залупу для", error_paste = "") => {
     for (let i = 0; i < 3; i++) {
         let body = await getResponsePorfirevich(text)
-        let result = (body.data.replies[rand_int(2, 0)]).trim()
-        if (
-            result.length > 8 &&
-            result.length < 38 &&
-            !result.includes("—") &&
-            !result.slice(-1) === "," &&
-            !result.split("\x20").slice(-1).toString().length <= 2
-        ) {
-            return result
+        let replies = body.data.replies
+        for (let j = 0; j < replies.length; j++) {
+            let result = (replies[j]).trim()
+            let len = result.length
+            if (
+                len > 8 && len < 38 &&
+                !result.includes("—") &&
+                !result.slice(-1) === "," &&
+                !result.split(
+                    "\x20")
+                    .slice(-1)
+                    .toString()
+                    .length <= 2
+            ) {
+                return result
+            }
         }
     }
     return error_paste
