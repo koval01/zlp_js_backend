cc9a7a91714c234327173b04952828849bfd4dd2
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-12-24 19:21:29 +0200

    update

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.post('/telegram/auth/check', rateLimit({
     max: 60,
     standardHeaders: true,
     message: rateLimitMessage
-}), tg_check, tg_check_view)
+}), tg_check, catchAsync(tg_check_view))
 
 app.post('/profile/skins/get', rateLimit({
     windowMs: 60 * 1000,
