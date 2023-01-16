1e7f103280aafdfb94b11ca70b5f216e5a0f56ef
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2023-01-16 22:12:19 +0200

    update

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const cors = require('cors')
 const express = require('express')
 const rateLimit = require('express-rate-limit')
 
-const Sentry = require("@sentry/node")
-const Tracing = require("@sentry/tracing")
-
 const log = require("./helpers/log")
 const {ip_get_view} = require("./helpers/methods")
 
@@ -29,15 +26,6 @@ const {events_view, channel_raw} = require("./helpers/telegram/channel")
 
 const app = express()
 
-Sentry.init({
-    dsn: "https://0df16c06d2d646b2be2b2ed4f7c967d0@o1226843.ingest.sentry.io/4504515915153408",
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({ app }),
-    ],
-    tracesSampleRate: 1.0
-})
-
 app.set('port', (process.env.PORT || 5000))
 app.set('trust proxy', parseInt(process.env.PROXY_LAYER))
 
@@ -45,9 +33,6 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(compression())
 app.use(cors())
-
-app.use(Sentry.Handlers.requestHandler())
-app.use(Sentry.Handlers.tracingHandler())
 
 app.use(log.logRequest)
 app.use(log.logError)
@@ -211,8 +196,6 @@ app.post('/server', rateLimit({
 app.get('*', async (_, resp) => {
     return main_e(resp, "error route", "This route cannot be found")
 })
-
-app.use(Sentry.Handlers.errorHandler())
 
 app.listen(app.get('port'), () => {
     console.debug(`Node app is running at localhost:${app.get('port')}`)
