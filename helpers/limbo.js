b5ab4a691ca560e7e62766b1ff3cbf4eff805d61
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2023-01-22 16:55:32 +0200

    update

--- helpers/limbo.js
+++ helpers/limbo.js
@@ -3,6 +3,10 @@ const struct = require('python-struct')
 const crypto = require('crypto')
 const {utf8_to_b64} = require('./methods')
 
+const ab2str = (arraybuffer) => {
+    return String.fromCharCode.apply(null, new Uint16Array(arraybuffer))
+}
+
 const generateSiphash = (player_username) => {
     const verify_key = "testkey8384398"
     const key = crypto.createHash('md5').update(verify_key).digest('hex')
@@ -13,7 +17,7 @@ const generateSiphash = (player_username) => {
 
     const tokenhash = siphash24(username_bytes+timestamp_bytes, key)
 
-    return utf8_to_b64(timestamp_bytes.join(tokenhash))
+    return utf8_to_b64(ab2str(timestamp_bytes.join(tokenhash)))
 }
 
 module.exports = {
