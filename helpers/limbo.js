4606dfa3d724f3e00439fae04bcfcc98e675e8db
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2023-01-22 16:05:18 +0200

    update

--- helpers/limbo.js
+++ helpers/limbo.js
@@ -4,13 +4,14 @@ const crypto = require('crypto')
 const {utf8_to_b64} = require('./methods')
 
 const generateSiphash = (player_username) => {
-    const verify_key = crypto.createHash('md5').update("testkey8384398").digest('hex')
+    const verify_key = "testkey8384398"
+    const key = crypto.createHash('md5').update(verify_key).digest('hex')
     const issue_timestamp = Date.now() / 1000
 
     const username_bytes = Buffer.from(player_username.toLowerCase(), 'utf-8').toString()
     const timestamp_bytes = struct.pack(">Q", issue_timestamp)
 
-    let tokenhash = siphash24(verify_key)
+    let tokenhash = siphash24(key)
     tokenhash.update(username_bytes)
     tokenhash.update(timestamp_bytes)
     tokenhash = tokenhash.hash()
