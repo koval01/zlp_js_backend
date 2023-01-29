090aab3dd36ab2bcb3e1ebb351d0925ed191ab5e
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2023-01-30 00:42:53 +0200

    update

--- database/functions/get_player.js
+++ database/functions/get_player.js
@@ -83,8 +83,9 @@ const get_player_auth = (callback, telegram_id) => {
                     } catch (_) {
                         player["SKIN"] = "31f477eb1a7beee631c2ca64d06f8f68fa93a3386d04452ab27f43acdf1b60cb" // Steve
                     }
-                    player["PREMIUM"] = (typeof player["PREMIUMUUID"] !== 'undefined' && player["PREMIUMUUID"].length)
+                    player["PREMIUM"] = typeof player["PREMIUMUUID"] !== 'undefined' && player["PREMIUMUUID"].length
                     delete player["PREMIUMUUID"]
+                    delete player["LOWERCASENICKNAME"]
                     get_private_server(function (private_) {
                         player["PRIVATE_SERVER"] = !!(
                             typeof private_ !== 'undefined' && private_.length
